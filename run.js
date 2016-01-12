1b8bce156a253b5ab57c49c5560af247a8870557
MxMcG <mmcgeevibes@gmail.com>
2016-01-12 10:33:45 -0800

    add new algo

--- run.js
+++ run.js
@@ -1,34 +1,48 @@
 // Merge Sort algorithm
 
-function mergeSort( array ) {
-  var len = array.length,
-      mid = Math.floor( len / 2 ),
-      left = array.slice( 0, mid ),
-      right = array.slice( mid, len )
-  if ( len < 2 ) {
-    return array;
-  }
-  return merge( mergeSort(left), mergeSort(right) );
+// function mergeSort( array ) {
+//   var len = array.length,
+//       mid = Math.floor( len / 2 ),
+//       left = array.slice( 0, mid ),
+//       right = array.slice( mid, len )
+//   if ( len < 2 ) {
+//     return array;
+//   }
+//   return merge( mergeSort(left), mergeSort(right) );
+// }
+
+// function merge ( left, right ) {
+//   var result = [],
+//       r_cnt = 0,
+//       l_cnt = 0,
+//       r_len = right.length,
+//       l_len = left.length
+//   while ( l_cnt < l_len && r_cnt < r_len ) {
+//     if ( left[l_cnt] < right[r_cnt]) {
+//       result.push(left[l_cnt++]);
+//     } else {
+//       result.push(right[r_cnt++]);
+//     }
+//   }
+//   return result.concat(left.slice(l_cnt)).concat(right.slice(r_cnt));
+// }
+
+// console.log(mergeSort([200000, 6, 4, 2, 5, 6, 4, 5, 6 ,7,77, 777]))
+
+
+
+// transofrm a array of objects into array of objects k, v
+
+function fun(array) {
+  var reformattedArray = array.map(function(obj){
+    var newObj = {};
+    newObj[obj.key] = String(obj.value);
+    return newObj;
+  });
+  return reformattedArray;
 }
 
-function merge ( left, right ) {
-  var result = [],
-      r_cnt = 0,
-      l_cnt = 0,
-      r_len = right.length,
-      l_len = left.length
-  while ( l_cnt < l_len && r_cnt < r_len ) {
-    if ( left[l_cnt] < right[r_cnt]) {
-      result.push(left[l_cnt++]);
-    } else {
-      result.push(right[r_cnt++]);
-    }
-  }
-  return result.concat(left.slice(l_cnt)).concat(right.slice(r_cnt));
-}
-
-console.log(mergeSort([200000, 6, 4, 2, 5, 6, 4, 5, 6 ,7,77, 777]))
-
+console.log(fun([{key:1, value:10}, {key:2, value:20}, {key:3, value: 30}]));
 
 // Permutation solution
 // you can write to stdout for debugging purposes, e.g.
--- test.rb
+++ test.rb
@@ -1,4 +1,3 @@
-
 def mergesort(array)
   len = array.length
   if len < 2
@@ -29,7 +28,32 @@ def merge (left, right)
   return results.concat(left.slice(l_count)).concat(right.slice(r_count))
 end
 
-
 p mergesort([3, 24, 5, 6, 4, 3])
+
+# binary gap algorithm
+
+def bin_gap(n)
+    bin = n.to_s(2)
+    bin_len = bin.length
+    bin_ary = bin.chars
+    gap_cnt_ary = []
+    bin_ary.each_with_index do |num, index|
+        if num == "1"
+            gap_cnt_ary << index
+        end
+    end
+    max_gap = 0
+    gap_cnt_ary.each_with_index do |n, index|
+        if index > 0
+            diff = (n - gap_cnt_ary[index-1]) -1
+            if diff > max_gap
+                max_gap = diff
+            end
+        end
+    end
+    return max_gap
+end
+
+bin_gap(324)
 
 
